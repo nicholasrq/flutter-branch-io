6b36639128310bd89d0ecaf95367e88b94889d7c
Nikita Skryabin <767890+nicholasrq@users.noreply.github.com>
2019-12-09 04:27:16 +0300

    formatting and types, fixing build

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/nicholasrq/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nicholasrq/Git/pickio/plugins/flutter-branch-io/example"
+export "FLUTTER_TARGET=/Users/nicholasrq/Git/pickio/plugins/flutter-branch-io/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/nicholasrq/development/flutter/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"
