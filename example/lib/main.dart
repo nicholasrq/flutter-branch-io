eb83993196c0d17efa48dc2913dc54f9e245b975
Nikita Skryabin <767890+nicholasrq@users.noreply.github.com>
2020-02-21 12:17:01 +0300

    fixes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_android_lifecycle/flutter_android_lifecycle.dart';
 import 'package:flutter_branch_io_plugin/flutter_branch_io_plugin.dart';
 
@@ -26,7 +26,6 @@ class _MyAppState extends State<MyApp> {
       setState(() {
         this.error = error.toString();
       });
-      print("BRANCH ERROR ${error.toString()}");
     }
   }
 
@@ -36,50 +35,26 @@ class _MyAppState extends State<MyApp> {
     }
 
     FlutterBranchIoPlugin.listenToDeepLinkStream().listen((string) {
-      print("DEEPLINK $string");
       setState(() {
         this._data = string;
       });
     });
     if (Platform.isAndroid) {
       FlutterAndroidLifecycle.listenToOnStartStream().listen((string) {
-        print("ONSTART");
         FlutterBranchIoPlugin.setupBranchIO();
       });
     }
 
     FlutterBranchIoPlugin.listenToGeneratedLinkStream().listen((link) {
-      print("GET LINK IN FLUTTER");
       print(link);
       setState(() {
         this.generatedLink = link;
       });
     });
 
-    FlutterBranchIoPlugin.generateLink(
-        FlutterBranchUniversalObject()
-            .setCanonicalIdentifier("content/12345")
-            .setTitle("My Content Title")
-            .setContentDescription("My Content Description")
-            .setContentImageUrl("https://lorempixel.com/400/400")
-            .setContentIndexingMode(BUO_CONTENT_INDEX_MODE.PUBLIC)
-            .setLocalIndexMode(BUO_CONTENT_INDEX_MODE.PUBLIC),
-        lpChannel: "facebook",
-        lpFeature: "sharing",
-        lpCampaign: "content 123 launch",
-        lpStage: "new user",
-        lpControlParams: {
-          "url": "http://www.google.com"
-        }
-    );
+    FlutterBranchIoPlugin.generateLink(FlutterBranchUniversalObject().setCanonicalIdentifier("content/12345").setTitle("My Content Title").setContentDescription("My Content Description").setContentImageUrl("https://lorempixel.com/400/400").setContentIndexingMode(BUO_CONTENT_INDEX_MODE.PUBLIC).setLocalIndexMode(BUO_CONTENT_INDEX_MODE.PUBLIC), lpChannel: "facebook", lpFeature: "sharing", lpCampaign: "content 123 launch", lpStage: "new user", lpControlParams: {"url": "http://www.google.com"});
 
-    FlutterBranchIoPlugin.trackContent( FlutterBranchUniversalObject()
-        .setCanonicalIdentifier("content/12345")
-        .setTitle("My Content Title")
-        .setContentDescription("My Content Description")
-        .setContentImageUrl("https://lorempixel.com/400/400")
-        .setContentIndexingMode(BUO_CONTENT_INDEX_MODE.PUBLIC)
-        .setLocalIndexMode(BUO_CONTENT_INDEX_MODE.PUBLIC), FlutterBranchStandardEvent.VIEW_ITEM);
+    FlutterBranchIoPlugin.trackContent(FlutterBranchUniversalObject().setCanonicalIdentifier("content/12345").setTitle("My Content Title").setContentDescription("My Content Description").setContentImageUrl("https://lorempixel.com/400/400").setContentIndexingMode(BUO_CONTENT_INDEX_MODE.PUBLIC).setLocalIndexMode(BUO_CONTENT_INDEX_MODE.PUBLIC), FlutterBranchStandardEvent.VIEW_ITEM);
   }
 
   @override
@@ -99,22 +74,18 @@ class _MyAppState extends State<MyApp> {
               children: <Widget>[
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text(
-                      "LATEST DATA BRANCH $_data"
-                  ),
+                  child: Text("LATEST DATA BRANCH $_data"),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text(
-                      "GENERATED LINK $generatedLink"
-                  ),
+                  child: Text("GENERATED LINK $generatedLink"),
                 ),
-                error.isEmpty ? Container() : Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text(
-                      "ERROR: $error"
-                  ),
-                )
+                error.isEmpty
+                    ? Container()
+                    : Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Text("ERROR: $error"),
+                      )
               ],
             ),
           );
